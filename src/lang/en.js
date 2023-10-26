42d7c14980636554511b9b0e73ebeb60d89dd9cd
lihe07 <li@imlihe.com>
2023-10-26 10:59:30 +0800

    update

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,55 +1,56 @@
 import codes from "./codes.en";
 
 export default {
-  index: {
-    ranking: {
-      title: "Carbon Map of China",
-      description:
-        "We are committed to provide concise carbon emission data for readers. By visualizing the abstract and tedious data to make reading more engaging, we wish to create personal connections between readers and carbon emissions in China.",
-    },
-    takeAction: {
-      title: "Take Action",
-      description:
-        "Here, we show the equation for converting carbon emissions to other substances",
-    },
-    articles: {
-      title: "Articles",
-      description:
-        "We provide articles (and summaries) on carbon dioxide emission and global warming, we hope readers can better understand greenhouse gas emissions and global warming,",
-    },
-    about: {
-      title: "About Us",
-      description: "We are the main founders of Carbinsight",
-    },
-    footer: {
-      otherOrgs: "Other Organizations",
-    },
-    title: "Home",
-  },
-  articles: {
-    title: "Articles",
-  },
-  ranking: {
-    title: "Ranking",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    rank: "Rank",
-    name: "School Name",
-    amount: "Amount",
-  },
-  map: {
-    title: "Map",
-    factors: "Factors",
-    satelite: "Satelite",
-    total: "Total",
-    perCapita: "Per Capita",
-    perGDP: "Per GDP",
-    dataSource: "Data Source",
-    dataSourceDescription: "Our data is processed from multiple data sources",
-  },
-  notFound: {
-    title: "Page Not Found",
-    description: "This page does not exist",
-  },
-  codes,
-  otherName: "中文",
+	index: {
+		ranking: {
+			title: "Carbon Map of China",
+			description:
+				"We are committed to provide concise carbon emission data for readers. By visualizing the abstract and tedious data to make reading more engaging, we wish to create personal connections between readers and carbon emissions in China.",
+		},
+		takeAction: {
+			title: "Take Action",
+			description:
+				"Here, we show the equation for converting carbon emissions to other substances",
+		},
+		articles: {
+			title: "Articles",
+			description:
+				"We provide articles (and summaries) on carbon dioxide emission and global warming, we hope readers can better understand greenhouse gas emissions and global warming,",
+		},
+		about: {
+			title: "About Us",
+			description: "We are the main founders of Carbinsight",
+		},
+		footer: {
+			otherOrgs: "Other Organizations",
+		},
+		title: "Home",
+	},
+	articles: {
+		title: "Articles",
+	},
+	ranking: {
+		title: "Ranking",
+		description:
+			"Apart from visualizing China’s carbon emission data, Carbinsight also hosts offline recycling programs. Every month in Beijing Academy, Carbinsights collects wastepaper and plastic bottles from every class and hand our prizes for the most avid recyclers. Meanwhile, Carbinsight also partners with different environmental clubs in high school around China, updating the ranking data every semester, hopping to encourage students to live a sustainable life. ",
+		rank: "Rank",
+		name: "School Name",
+		amount: "Amount",
+	},
+	map: {
+		title: "Map",
+		factors: "Factors",
+		satelite: "Satelite",
+		total: "Total",
+		perCapita: "Per Capita",
+		perGDP: "Per GDP",
+		dataSource: "Data Source",
+		dataSourceDescription: "Our data is processed from multiple data sources",
+	},
+	notFound: {
+		title: "Page Not Found",
+		description: "This page does not exist",
+	},
+	codes,
+	otherName: "中文",
 };
--- src/lang/zh.js
+++ src/lang/zh.js
@@ -1,55 +1,56 @@
 import codes from "./codes.zh";
 
 export default {
-  index: {
-    firstBlock: {},
-    ranking: {
-      title: "全国碳地图",
-      description:
-        "我们致力于将枯燥的数据数据简洁地呈现给读者，使读者和碳排放之间建立个人联系。并将抽象枯燥的数据视图化，使得阅读更加轻松有趣。",
-    },
-    takeAction: {
-      title: "采取行动",
-      description: "在这里我们会体现出碳排放量与其他物质的转化等式",
-    },
-    articles: {
-      title: "文章",
-      description:
-        "我们提供了介绍二氧化碳排放和全球变暖的文章（和概括），希望读者能更好的理解温室气体和全球变暖。",
-    },
-    about: {
-      title: "关于我们",
-      description: "我们是Carbinsight项目的发起者",
-    },
-    footer: {
-      otherOrgs: "其他组织",
-    },
-    title: "首页",
-  },
-  articles: {
-    title: "文章",
-  },
-  ranking: {
-    title: "学校排名",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    rank: "排名",
-    name: "学校名称",
-    amount: "回收量",
-  },
-  map: {
-    title: "地图",
-    factors: "排放因子",
-    satelite: "卫星",
-    total: "总量",
-    perCapita: "人均",
-    perGDP: "按GDP",
-    dataSource: "数据来源",
-    dataSourceDescription: "我们的数据由多家组织数据源经过处理得到",
-  },
-  notFound: {
-    title: "找不到内容",
-    description: "这个页面不存在",
-  },
-  codes,
-  otherName: "English",
+	index: {
+		firstBlock: {},
+		ranking: {
+			title: "全国碳地图",
+			description:
+				"我们致力于将枯燥的数据数据简洁地呈现给读者，使读者和碳排放之间建立个人联系。并将抽象枯燥的数据视图化，使得阅读更加轻松有趣。",
+		},
+		takeAction: {
+			title: "采取行动",
+			description: "在这里我们会体现出碳排放量与其他物质的转化等式",
+		},
+		articles: {
+			title: "文章",
+			description:
+				"我们提供了介绍二氧化碳排放和全球变暖的文章（和概括），希望读者能更好的理解温室气体和全球变暖。",
+		},
+		about: {
+			title: "关于我们",
+			description: "我们是Carbinsight项目的发起者",
+		},
+		footer: {
+			otherOrgs: "其他组织",
+		},
+		title: "首页",
+	},
+	articles: {
+		title: "文章",
+	},
+	ranking: {
+		title: "学校排名",
+		description:
+			"Carbinsight 项目除了显示中国碳排放数据外，还开启了线下的回收活动。在北京中学校园内，Carbinsight每月收集统计各班废纸和塑料瓶，为回收最多的班级颁奖。同时，Carbinsight和中国多所高中环保社团合作，每学期更新回收排名数据，希望能激励同学们参与低碳生活。",
+		rank: "排名",
+		name: "学校名称",
+		amount: "回收量",
+	},
+	map: {
+		title: "地图",
+		factors: "排放因子",
+		satelite: "卫星",
+		total: "总量",
+		perCapita: "人均",
+		perGDP: "按GDP",
+		dataSource: "数据来源",
+		dataSourceDescription: "我们的数据由多家组织数据源经过处理得到",
+	},
+	notFound: {
+		title: "找不到内容",
+		description: "这个页面不存在",
+	},
+	codes,
+	otherName: "English",
 };
